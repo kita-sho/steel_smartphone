e40ae054b68047483fcaa5bed0a2250e7b3700f6
takumi2003 <takumi20030819@gmail.com>
2024-12-05 13:56:41 +0900

    更新

--- CSV2HTML_by_Java/csv2html/Table.java
+++ CSV2HTML_by_Java/csv2html/Table.java
@@ -9,151 +9,154 @@ import utility.ImageUtility;
 /**
  * 表：情報テーブル。
  */
-public class Table extends Object
-{
-	/**
-	 * 属性リストを記憶するフィールド。
-	 */
-	private Attributes attributes;
+public class Table extends Object {
 
-	/**
-	 * タプル群を記憶するフィールド。
-	 */
-	private List<Tuple> tuples;
+    /**
+     * 属性リストを記憶するフィールド。
+     */
+    private Attributes attributes;
 
-	/**
-	 * 画像群を記憶するフィールド。
-	 */
-	private List<BufferedImage> images;
+    /**
+     * タプル群を記憶するフィールド。
+     */
+    private List<Tuple> tuples;
 
-	/**
-	 * サムネイル画像群を記憶するフィールド。
-	 */
-	private List<BufferedImage> thumbnails;
+    /**
+     * 画像群を記憶するフィールド。
+     */
+    private List<BufferedImage> images;
 
-	/**
-	 * テーブルのコンストラクタ。
-	 * @param instanceOfAttributes 属性リスト
-	 */
-	public Table(Attributes instanceOfAttributes)
-	{
-		super();
+    /**
+     * サムネイル画像群を記憶するフィールド。
+     */
+    private List<BufferedImage> thumbnails;
 
-		this.attributes = instanceOfAttributes;
-		this.tuples = null;
-		this.images = null;
-		this.thumbnails = null;
+    /**
+     * テーブルのコンストラクタ。
+     *
+     * @param instanceOfAttributes 属性リスト
+     */
+    public Table(Attributes instanceOfAttributes) {
+        super();
 
-		return;
-	}
+        this.attributes = instanceOfAttributes;
+        this.tuples = null;
+        this.images = null;
+        this.thumbnails = null;
 
-	/**
-	 * タプルを追加する。
-	 * @param aTuple タプル
-	 */
-	public void add(Tuple aTuple)
-	{
-		this.tuples().add(aTuple);
+        return;
+    }
 
-		return;
-	}
+    /**
+     * タプルを追加する。
+     *
+     * @param aTuple タプル
+     */
+    public void add(Tuple aTuple) {
+        this.tuples().add(aTuple);
 
-	/**
-	 * 属性リストを応答する。
-	 * @return 属性リスト
-	 */
-	public Attributes attributes()
-	{
-		return this.attributes;
-	}
+        return;
+    }
 
-	/**
-	 * 属性リストを設定する。
-	 * @param instanceOfAttributes 属性リスト
-	 */
-	public void attributes(Attributes instanceOfAttributes)
-	{
-		this.attributes = instanceOfAttributes;
+    /**
+     * 属性リストを応答する。
+     *
+     * @return 属性リスト
+     */
+    public Attributes attributes() {
+        return this.attributes;
+    }
 
-		return;
-	}
+    /**
+     * 属性リストを設定する。
+     *
+     * @param instanceOfAttributes 属性リスト
+     */
+    public void attributes(Attributes instanceOfAttributes) {
+        this.attributes = instanceOfAttributes;
 
-	/**
-	 * 画像群を応答する。
-	 * @return 画像群
-	 */
-	public List<BufferedImage> images()
-	{
-		if (this.images != null) { return this.images; }
-		this.images = new ArrayList<BufferedImage>();
-		for (Tuple aTuple : this.tuples())
-		{
-			String aString = aTuple.values().get(aTuple.attributes().indexOfImage());
-			BufferedImage anImage = this.picture(aString);
-			this.images.add(anImage);
-		}
+        return;
+    }
 
-		return this.images;
-	}
+    /**
+     * 画像群を応答する。
+     *
+     * @return 画像群
+     */
+    public List<BufferedImage> images() {
+        if (this.images != null) {
+            return this.images;
+        }
+        this.images = new ArrayList<BufferedImage>();
+        for (Tuple aTuple : this.tuples()) {
+            String aString = aTuple.values().get(aTuple.attributes().indexOfImage());
+            BufferedImage anImage = this.picture(aString);
+            this.images.add(anImage);
+        }
 
-	/**
-	 * 画像またはサムネイル画像の文字列を受けとって当該画像を応答する。
-	 * @param aString 画像またはサムネイル画像の文字列
-	 * @return 画像
-	 */
-	private BufferedImage picture(String aString)
-	{
-		return null;
-	}
+        return this.images;
+    }
 
-	/**
-	 * サムネイル画像群を応答する。
-	 * @return サムネイル画像群
-	 */
-	public List<BufferedImage> thumbnails()
-	{
-		if (thumbnails != null) { return this.thumbnails; }
-		this.thumbnails = new ArrayList<BufferedImage>();
-		for (Tuple aTuple : this.tuples())
-		{
-			String aString = aTuple.values().get(aTuple.attributes().indexOfThumbnail());
-			BufferedImage anImage = this.picture(aString);
-			this.thumbnails.add(anImage);
-		}
+    /**
+     * 画像またはサムネイル画像の文字列を受けとって当該画像を応答する。
+     *
+     * @param aString 画像またはサムネイル画像の文字列
+     * @return 画像
+     */
+    private BufferedImage picture(String aString) {
+        return null;
+    }
 
-		return this.thumbnails;
-	}
+    /**
+     * サムネイル画像群を応答する。
+     *
+     * @return サムネイル画像群
+     */
+    public List<BufferedImage> thumbnails() {
+        if (thumbnails != null) {
+            return this.thumbnails;
+        }
+        this.thumbnails = new ArrayList<BufferedImage>();
+        for (Tuple aTuple : this.tuples()) {
+            String aString = aTuple.values().get(aTuple.attributes().indexOfThumbnail());
+            BufferedImage anImage = this.picture(aString);
+            this.thumbnails.add(anImage);
+        }
 
-	/**
-	 * 自分自身を文字列にして、それを応答する。
-	 * @return 自分自身の文字列
-	 */
-	public String toString()
-	{
-		StringBuffer aBuffer = new StringBuffer();
-		Class<?> aClass = this.getClass();
-		aBuffer.append(aClass.getName());
-		aBuffer.append("[");
-		aBuffer.append(this.attributes());
-		for (Tuple aTuple : this.tuples())
-		{
-			aBuffer.append(",");
-			aBuffer.append(aTuple);
-		}
-		aBuffer.append("]");
+        return this.thumbnails;
+    }
 
-		return aBuffer.toString();
-	}
+    /**
+     * 自分自身を文字列にして、それを応答する。
+     *
+     * @return 自分自身の文字列
+     */
+    public String toString() {
+        StringBuffer aBuffer = new StringBuffer();
+        Class<?> aClass = this.getClass();
+        aBuffer.append(aClass.getName());
+        aBuffer.append("[");
+        aBuffer.append(this.attributes());
+        for (Tuple aTuple : this.tuples()) {
+            aBuffer.append(",");
+            aBuffer.append(aTuple);
+        }
+        aBuffer.append("]");
 
-	/**
-	 * タプル群を応答する。
-	 * @return タプル群
-	 */
-	public List<Tuple> tuples()
-	{
-		if (this.tuples != null) { return this.tuples; }
-		this.tuples = new ArrayList<Tuple>();
+        return aBuffer.toString();
+    }
 
-		return this.tuples;
-	}
+    /**
+     * タプル群を応答する。
+     *
+     * @return タプル群
+     */
+    public List<Tuple> tuples() {
+        if (this.tuples != null) {
+            return this.tuples;
+        }
+        this.tuples = new ArrayList<Tuple>();
+
+        return this.tuples;
+    }
 }
